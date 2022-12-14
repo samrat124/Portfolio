6b0d7287b30924ef461a8240506423262647de3a
Samrat Kumar <sam9999950@gmail.com>
2022-12-14 20:32:39 +0530

    portfolio

---
+++ developer-portfolio-master/src/data/experienceData.js
@@ -0,0 +1,23 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'Masai School',
+        jobtitle: 'Full Stack Web Developer',
+        startYear: 'CODING',
+        endYear: 'More Than 1200 hours Coding Experience'
+    },
+    {
+        id: 2,
+        company: 'Masai School',
+        jobtitle: 'Full Stack Web Developer',
+        startYear: 'DSA',
+        endYear: 'More Than 500 Questions Solved'
+    },
+    // {
+    //     id: 3,
+    //     company: 'Wonka Industries',
+    //     jobtitle: 'Marketing',
+    //     startYear: '2020',
+    //     endYear: 'Present'
+    // },
+]
---
+++ developer-portfolio-master/src/data/projectsData.js
@@ -0,0 +1,100 @@
+import one from '../assets/svg/projects/one.svg'
+import two from '../assets/svg/projects/two.svg'
+import three from '../assets/svg/projects/three.svg'
+import four from '../assets/svg/projects/four.svg'
+import five from '../assets/svg/projects/five.svg'
+import six from '../assets/svg/projects/six.svg'
+import seven from '../assets/svg/projects/seven.svg'
+import eight from '../assets/svg/projects/eight.svg'
+
+
+export const projectsData = [
+    {
+        id: 1,
+        projectName: 'DominoS Clone',
+        projectDesc: 'This project aims to build an  online food and Restaurant management system that consolidates all information of a company',
+        tags: ['React', 'CSS', 'Material Ui','Chakra Ui','React-BootStrap'],
+        code: 'https://github.com/priyakesarwani0101/Domino-s_Clone',
+        demo: 'https://startling-kitsune-fc132d.netlify.app/',
+        image: one
+    },
+    {
+        id: 2,
+        projectName: 'BlissClub Clone',
+        projectDesc: 'This project is a E-Commerce system that allows users to obtain relevant information and order online.',
+        tags: ['Html','CSS','Java-Script','BootStrap'],
+        code: 'https://github.com/PankuMehra/BlissClub-Clone',
+        demo: 'https://dainty-brigadeiros-8eb98a.netlify.app/',
+        image: two
+    },
+    {
+        id: 3,
+        projectName: 'PharmEasy Clone',
+        projectDesc: ' This Project is a E-Commerce system of medicines that allows user to provide medicine at door-step.',
+        tags: ['Html','CSS','Java-Script','BootStrap'],
+        code: 'https://github.com/samrat124/PharmEasy-Clone?organization=samrat124&organization=samrat124',
+        demo: 'https://pharmeasyclone2022.netlify.app/',
+        image: three
+    },
+    // {
+    //     id: 4,
+    //     projectName: 'Android Patient Tracker',
+    //     projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
+    //     tags: ['Flutter', 'Firebase'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: four
+    // },
+    // {
+    //     id: 5,
+    //     projectName: 'E-Commerce App',
+    //     projectDesc: 'A Simple E-commerce application',
+    //     tags: ['React Native', 'Firebase'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: five
+    // },
+    // {
+    //     id: 6,
+    //     projectName: 'Uber Lite',
+    //     projectDesc: 'Uber clone',
+    //     tags: ['Flutter'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: six
+    // },
+    // {
+    //     id: 7,
+    //     projectName: 'Stock Market App',
+    //     projectDesc: 'A simple stock market API app',
+    //     tags: ['React', 'Redux', 'Bootstrap'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: seven
+    // },
+    // {
+    //     id: 8,
+    //     projectName: 'Car Pooling System',
+    //     projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
+    //     tags: ['Flutter', 'React'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: eight
+    // },
+]
+
+
+// Do not remove any fields.
+// Leave it blank instead as shown below
+
+/* 
+{
+    id: 1,
+    projectName: 'Car Pooling System',
+    projectDesc: '',
+    tags: ['Flutter', 'React'],
+    code: '',
+    demo: '',
+    image: ''
+}, 
+*/
