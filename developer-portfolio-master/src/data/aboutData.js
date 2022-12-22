3dbf8a40ccc7108c40ed65dd8401427ff3fcac17
Samrat Kumar <sam9999950@gmail.com>
2022-12-22 09:23:19 +0530

    some changes

--- developer-portfolio-master/src/data/aboutData.js
+++ developer-portfolio-master/src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name's Samrat . I'm a web designer and developer based in Bihar, India.",
-    description2: "Strong in design and integration with intuitive problem-solving skills. Proficient in HTML, CSS, JAVASCRIPT, REACT, and REACT-REDUX.Passionate about implementing and launching new projects. Ability to translate business requirements into technical solutions.Looking to start the career as an entry-level Web Developer.",
+    description2: "Strong in design and integration with intuitive problem-solving skills. Proficient in Html , Css, Javascript, React, and Redux.Passionate about implementing and launching new projects. Ability to translate business requirements into technical solutions.Looking to start the career as an entry-level Web Developer.",
     image: 2
 }
