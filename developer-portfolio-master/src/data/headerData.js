af23bad6f39cef109f0f7a5cc96d2b11fa8c87ec
samrat kumar <sam9999950@gmail.com>
2023-01-18 16:48:14 +0530

    header

--- developer-portfolio-master/src/data/headerData.js
+++ developer-portfolio-master/src/data/headerData.js
@@ -3,7 +3,7 @@ import Samrat_Kumar_Resume from '../assets/pdf/Samrat_Kumar_Resume.pdf'
 export const headerData = {
     name: 'Samrat Kumar',
     title: "Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    desciption:"What is the recipe for successful achievement? To my mind there are just four essential ingredients: Choose a career you love, give it the best there is in you, seize your opportunities, and be a member of the team. ",
     image: 'https://avatars.githubusercontent.com/u/111118961?v=4',
     resumePdf: Samrat_Kumar_Resume
 }
